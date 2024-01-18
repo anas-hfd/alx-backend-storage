a8603cf8f581388b9410432e8a5a64b2f6608982
anas-hfd <anas1.hfd@gmail.com>
2024-01-18 13:12:52 +0100

    users table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 --- creates a table of users
 CREATE TABLE IF NOT EXISTS users (
-    --- attributes:
+    --- attributes
     id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     PRIMARY KEY (id)
-)
+);
